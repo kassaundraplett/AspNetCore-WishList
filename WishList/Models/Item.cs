50715425b36121a512ceab6486c3fbcb5e9f06a2
Kassaundra Plett <kass-p@EMCINS.COM>
2020-06-02 21:10:49 -0500

    Completed module

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required()]
+        public string Description { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
